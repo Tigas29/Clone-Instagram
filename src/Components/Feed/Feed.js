468a352a8bd22590fb0a44926620f47234bd17a8
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2022-05-04 19:25:32 -0300

    commit

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -10,60 +10,65 @@ import Logo from "../../img/logopost1.jpg";
 export function Feed(props) {
   return (
     <>
-      <div className="containter-post">
-        <div className="infos-post">
-          <img className="Logo-cabecalho" src={props.Logo} />
-          <p>{props.nomeLogo}</p>
-        </div>
-        <FiMoreHorizontal />
-      </div>
-      <div className="img-post">
-        <img src={props.PostoPhoto} />
-      </div>
-      <div className="infos-footer-post">
-        <IconContext.Provider value={{ size: "30px" }}>
-          <div className="icons-container">
-            <div className="icons1">
-              <div className="icon">
-                <IoMdHeartEmpty />
-              </div>
-              <div className="icon">
-                <BsChat />
-              </div>
-              <div className="icon">
-                <FiSend />
-              </div>
-            </div>
-            <div className="icons-right">
-              <BsBookmark />
-            </div>
+      <div className="teste">
+        <div className="containter-post">
+          <div className="infos-post">
+            <img className="Logo-cabecalho" src={props.Logo} />
+            <p>{props.nomeLogo}</p>
           </div>
-        </IconContext.Provider>
-      </div>
-
-      <section className="like">
-        <span>{props.curtidas}</span>
-      </section>
-
-      <section className="legend">
-        <p>
-          <span>{props.nomeLogo}</span> {props.legenda}
-        </p>
-      </section>
-      <section className="time-post">
-        <time>HÁ 1 HORA</time>
-      </section>
-
-      <div className="input-fake-comment">
-        <div className="fake-input">
-          <IconContext.Provider value={{ size: "25px" }}>
-            <div className="icon-comment">
-              <BsEmojiSmile />
+          <FiMoreHorizontal />
+        </div>
+        <div className="img-post">
+          <img src={props.PostoPhoto} />
+        </div>
+        <div className="infos-footer-post">
+          <IconContext.Provider value={{ size: "30px" }}>
+            <div className="icons-container">
+              <div className="icons1">
+                <div className="icon">
+                  <IoMdHeartEmpty />
+                </div>
+                <div className="icon">
+                  <BsChat />
+                </div>
+                <div className="icon">
+                  <FiSend />
+                </div>
+              </div>
+              <div className="icons-right">
+                <BsBookmark />
+              </div>
             </div>
           </IconContext.Provider>
-          <input classnametype="comment" placeholder="Adicione um comentario" />
         </div>
-        <button>Publicar</button>
+
+        <section className="like">
+          <span>{props.curtidas}</span>
+        </section>
+
+        <section className="legend">
+          <p>
+            <span>{props.nomeLogo}</span> {props.legenda}
+          </p>
+        </section>
+        <section className="time-post">
+          <time>HÁ 1 HORA</time>
+        </section>
+
+        <div className="input-fake-comment">
+          <div className="fake-input">
+            <IconContext.Provider value={{ size: "25px" }}>
+              <div className="icon-comment">
+                <BsEmojiSmile />
+              </div>
+            </IconContext.Provider>
+            <input
+              classnametype="comment"
+              placeholder="Adicione um comentario"
+            />
+          </div>
+          <button>Publicar</button>
+        </div>
       </div>
     </>
   );
